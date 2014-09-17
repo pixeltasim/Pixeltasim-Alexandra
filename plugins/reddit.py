435eecec9544e011bc4195251e944f2b3b6c5901
pxdnbluesoul <daniel.tharp@gmail.com>
2014-09-16 20:20:19 -0600

    JSON-friendly Reddit.py
    Using Reddit's JSON interface to be less of a hog and obeying User-Agent
    identification.

--- plugins/reddit.py
+++ plugins/reddit.py
@@ -1,18 +1,30 @@
-from util import hook, http
+import json
+import urllib2
+import time
+from util import hook
 import re
 
 reddit_re = (r'.*((www\.)?reddit\.com/r[^ ]+)', re.I)
 
 @hook.regex(*reddit_re)
 def reddit_url(match):
+	
+    jsonlink = match.group(0) + '.json' # retrieve JSON-ified version of link
+    req = urllib2.Request(jsonlink)
+    req.add_header('User-agent', 'Python/Alexandra-1.0') # Reddit wants us to use unique user-agents. OK.
+    response = urllib2.urlopen(req)
 
-    thread = http.get_html(match.group(0))
-
-    title = thread.xpath('//title/text()')[0]
-    score = thread.xpath("//div[@class='score']/span[@class='number']/text()")[0]
-    author = thread.xpath("//div[@id='siteTable']//a[contains(@class,'author')]/text()")[0]
-    timeago = thread.xpath("//div[@id='siteTable']//p[@class='tagline']/time/text()")[0]
-    comments = thread.xpath("//div[@id='siteTable']/div[1]/div[2]/ul/li[1]/a/text()")[0]
-
-    return '%s - \x02%s\x02 - posted by \x02%s\x02 %s - %s net upvotes and %s' % (
-            match.group(0), title, author, timeago, score, comments)
+    data = json.load(response)
+    submission = data[0]['data']['children'][0]['data'] # Dig down to the relevant bits.
+    self = submission["is_self"]
+    if self == True:
+        url = 'http://redd.it/' + submission["id"]
+    else:
+        url = submission["url"]
+    title = submission["title"]
+    score = submission["score"]
+    author = submission["author"]
+    timeago = time.strftime("%b %d %Y %H:%M:%S", time.gmtime(submission["created_utc"]))
+    comments = submission["num_comments"]
+    return '%s - \x02%s\x02 - posted by \x02%s\x02 %s GMT - %s points and %s comments' % (
+    url, title, author, timeago, score, comments) 
